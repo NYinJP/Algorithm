862aae465d7c250701343e63ebd1867bbe885d24
이난영 <105438919+NYinJP@users.noreply.github.com>
2023-07-05 13:45:50 +0900

    Update 배열 원소의 길이.java

--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.java
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -1,8 +1,5 @@
 class Solution {
     public int[] solution(String[] strlist) {
-//        N 입력받기
-//        Scanner sc = new Scanner(System.in);
-//        String[] strlist = {"we","are","love"};
         
         int count = strlist.length;
 
@@ -11,6 +8,5 @@ class Solution {
             numArr[i] = strlist[i].length();
         }
         return numArr;
-        //System.out.println(Arrays.toString(numArr));
     }
 }
