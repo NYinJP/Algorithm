0be0d451b6c6b5bf57c4239f2c549db04e2e4fbb
이난영 <105438919+NYinJP@users.noreply.github.com>
2023-07-09 06:57:16 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public int solution(int price) {
+        if(price>=500_000) price *= 0.80;
+        else if(price >= 300_000) price *= 0.9;
+        else if(price >=100_000) price *=0.95;
+        
+        return price;
+        
+    }
+}
