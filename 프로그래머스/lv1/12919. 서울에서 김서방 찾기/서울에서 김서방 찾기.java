2bd099db3409275e2a4f82bb72cf595770c70ea1
이난영 <105438919+NYinJP@users.noreply.github.com>
2023-07-10 02:07:13 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 6.01 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -4,6 +4,7 @@ class Solution {
         for(int i=0;i<seoul.length;i++){
             if(seoul[i].equals("Kim")){
                 index = i;
+                break;
             }
         }return "김서방은 " + index+"에 있다";
     }
