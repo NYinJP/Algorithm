d61b5374345414a9900b2ec1fa41ffe91a592329
이난영 <105438919+NYinJP@users.noreply.github.com>
2023-07-09 17:51:13 +0900

    [level 0] Title: 약수 구하기, Time: 1.92 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -1,13 +1,9 @@
 import java.util.*;
 class Solution {
     public int[] solution(int n) {
-         int [] A = new int[n+1];
-        for(int i=1;i<=n;i++){
-            A[i]=i;
-        }
         List<Integer> answer = new ArrayList<Integer>();
         for (int i = 1; i <= n; i++) {
-            if(n%A[i] == 0) answer.add(i);
+            if(n%i == 0) answer.add(i);
         }
         return answer.stream().mapToInt(x->x).toArray();
     }
