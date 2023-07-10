517115f2d9ee4c529c013efd670a2b6427a6c39c
이난영 <105438919+NYinJP@users.noreply.github.com>
2023-07-10 23:31:08 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int sum =0; int square = 1;
+        for(int i=0; i < num_list.length;i++){
+            sum+=num_list[i];
+            square*=num_list[i];
+        }
+        return (square<sum*sum)?1:0;
+    }
+}
