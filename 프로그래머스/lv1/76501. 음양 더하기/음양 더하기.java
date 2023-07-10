c74021d29ebbae56c816ee4ce70f7ad7123acaf4
이난영 <105438919+NYinJP@users.noreply.github.com>
2023-07-10 17:02:35 +0900

    [level 1] Title: 음양 더하기, Time: 0.05 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -1,19 +1,24 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
-        int[] result = new int[absolutes.length];
-        for(int i=0;i<absolutes.length;i++){
+//         int[] result = new int[absolutes.length];
+//         for(int i=0;i<absolutes.length;i++){
             
-            String sign = "";
+//             String sign = "";
             
-            if(signs[i]) sign+="+";
-            else sign+="-";
-            result[i] = Integer.parseInt(sign+absolutes[i]);    
-        }
-        int sum =0;
-        for(int i=0;i<result.length;i++){
-            sum+=result[i];
-        }
-        return sum;
+//             if(signs[i]) sign+="+";
+//             else sign+="-";
+//             result[i] = Integer.parseInt(sign+absolutes[i]);    
+//         }
+//         int sum =0;
+//         for(int i=0;i<result.length;i++){
+//             sum+=result[i];
+//         }
+//         return sum;
+        
+        int answer = 0;
+        for (int i=0; i<signs.length; i++)
+            answer += absolutes[i] * (signs[i]? 1: -1);
+        return answer;
         
     }
 }
