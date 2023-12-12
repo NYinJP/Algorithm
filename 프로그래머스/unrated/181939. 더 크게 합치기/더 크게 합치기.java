39e4289514c85f5a59d2e8143e02db73cdab23a5
이난영 <105438919+NYinJP@users.noreply.github.com>
2023-12-12 12:59:53 +0900

    [level unrated] Title: 더 크게 합치기, Time: 7.08 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,12 +1,12 @@
 class Solution {
     public int solution(int a, int b) {
-        String aStr = a+"";
-        String bStr = b+"";
-        String aConcatB = aStr + bStr;
-        String bConcatA = bStr + aStr;
+        // String aStr = a+"";
+        // String bStr = b+"";
+        // String aConcatB = aStr + bStr;
+        // String bConcatA = bStr + aStr;
         
-        int ab = Integer.parseInt(aConcatB);
-        int ba = Integer.parseInt(bConcatA);
+        int ab = Integer.parseInt(a+""+b);
+        int ba = Integer.parseInt(b+""+a);
         return ab>ba? ab:ba;
         
     }
