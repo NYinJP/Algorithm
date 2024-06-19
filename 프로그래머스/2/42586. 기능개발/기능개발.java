b7ae1769d33cb2ec8d603e84c9e74be37748059b
이난영 <105438919+NYinJP@users.noreply.github.com>
2024-06-19 14:37:28 +0900

    [level 2] Title: 기능개발, Time: 2.54 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+LinkedList<Integer> queue = new LinkedList<>();
+        LinkedList<Integer> queueSpeed = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+
+        for(int i: progresses){
+            queue.add(i);
+        }
+        for (int i : speeds) {
+            queueSpeed.add(i);
+        }
+
+        int answer = 0; // 선언 및 초기화
+        while(!queue.isEmpty()) {
+            for (int i = 0; i < queue.size(); i++) {
+                int element = queue.poll();
+                int speed = queueSpeed.poll();
+                queue.add(element + speed);
+                queueSpeed.add(speed);
+            }
+
+            // 맨 끝 자리가 100 이상일 때,
+            if(queue.peek()>=100) {
+                while(true) {
+                    queue.poll();queueSpeed.poll();
+                    answer++;
+                    if(queue.isEmpty()) break;
+                    if(queue.peek()<100) break;
+                }
+                result.add(answer);
+                answer = 0;
+            }
+        }
+
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+}
