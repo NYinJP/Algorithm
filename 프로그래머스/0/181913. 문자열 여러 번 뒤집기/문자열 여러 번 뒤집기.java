c1d183e5c66d29f872db1f5114ace8e43669174d
이난영 <105438919+NYinJP@users.noreply.github.com>
2026-01-08 10:21:30 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 16.35 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -1,48 +1,41 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 class Solution {
     public String solution(String my_string, int[][] queries) {
-        char[] temp = my_string.toCharArray();
+        
         List<String> list = new ArrayList<>();
-        for (int i = 0; i < temp.length; i++) {
-            list.add(temp[i] + "");
+        for (char c : my_string.toCharArray()) {
+            list.add(String.valueOf(c));
         }
+
 
         for (int i = 0; i < queries.length; i++) {
 
+           
             int swapCount = (queries[i][1] - queries[i][0] +1)/2;
 
             int count = 0;
-
-            int k=0;
-
             for (int j = queries[i][0]; j <= queries[i][1]; j++) {
 
                 String temp1 = "";
-                String temp2 = ""; // 문자열 거꾸로 뒤집기. 맨 뒤의 값 가져오기
+                String temp2 = ""; 
 
-                temp1 = list.get(queries[i][1] - k); // 뒤의 값 b를
-                temp2 = list.get(j); //
+                temp1 = list.get(queries[i][1] - count);
+                temp2 = list.get(j);
 
-                list.set(j, temp1); // j는 0
-                list.set(queries[i][1] - k, temp2);
+                list.set(j, temp1); 
+                list.set(queries[i][1] - count, temp2); 
 
                 count ++;
-                k++;
 
+              
                 if (count == swapCount) {
                     break;
                 }
             }
-
-
         }
 
-        String result = "";
-        for (int i = 0; i < list.size(); i++) {
-            result += list.get(i);
-        }
-
-        return result;
+        return list.stream().collect(Collectors.joining());
     }
 }
