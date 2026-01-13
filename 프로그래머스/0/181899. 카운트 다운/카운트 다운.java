1854d00c7a0632333c434a52694110475f904963
이난영 <105438919+NYinJP@users.noreply.github.com>
2026-01-13 17:51:13 +0900

    [level 0] Title: 카운트 다운, Time: 0.03 ms, Memory: 91.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        
+        int[] result = new int[start_num - end_num + 1];
+
+        for (int i = start_num; i >= end_num; i--) {
+
+            result[start_num -i] = i;
+        }
+
+        return result;
+    }
+}
